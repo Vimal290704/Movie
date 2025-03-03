b1080c03c930cdb22b3ec72b765743f9eb86ac92
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-03-03 10:49:29 +0530

    yo

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <a href="">Logo</a>
+      <a href="">Movies</a>
+      <a href="">WatchList</a>
+      <a href="">Movie Recommendation</a>
+    </div>
+  );
+};
+
+export default Navbar;
