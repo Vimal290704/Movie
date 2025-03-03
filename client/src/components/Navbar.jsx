1875354d559ec00ab5e473dcc85d31762b839347
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-03-03 11:06:37 +0530

    j

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <a href="">Logo</a>
+    <div className="text-sky-600 text-3xl font-bold flex gap-16 mt-1">
+      <a href="">
+        <img src="../assets/image.png" alt="" />
+      </a>
       <a href="">Movies</a>
       <a href="">WatchList</a>
       <a href="">Movie Recommendation</a>
